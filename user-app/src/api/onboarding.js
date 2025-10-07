3382c71f9bba47eeeacff6b7fa889ed702c4ac2c
TupiiPae <tuanphi1509@gmail.com>
2025-10-07 22:46:41 +0700

    Onboard Login - Succes first time

--- user-app/src/api/onboarding.js
+++ user-app/src/api/onboarding.js
@@ -1,6 +1,5 @@
 import api from "./user-api";
 
-// Gửi dữ liệu tiếng Việt như schema để server tính BMI/BMR
 export async function upsertOnboarding(payload) {
   const { data } = await api.post("/api/user/onboarding/upsert", payload);
   return data.data; // Onboarding doc
